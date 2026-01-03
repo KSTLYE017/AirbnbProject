b46f79cf9a84d9b2736f870c3d345f0ced36a91d
Krisha patel <krishapatel172422@gmail.com>
2026-01-03 12:28:20 +0530

    Update required changes

--- app.js
+++ app.js
@@ -42,8 +42,6 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-
-
 const store=MongoStore.create({
     mongoUrl: dburl,
     crypto: {
